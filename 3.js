31d7c562a23a4f09aca197121bae04b80e9cc56e
ciribas <caroiribas@hotmail.com>
2024-08-11 12:56:26 -0300

    cargar

--- 3.js
+++ 3.js
@@ -14,3 +14,29 @@ En caso de que el usuario ingrese un valor distinto, mostrarle el
 mensaje: "Ingresaste un valor inválido".
 Finalmente, agradecer al usuario por su visita.*/
 
+const prompt = require("prompt-sync")({ sigint: true }); 
+
+let valoracion=parseFloat(prompt("Ingrese la valoracion para la pelicula: "));
+
+switch(valoracion){
+    case 1: 
+    console.log("Calificaste la pelicula como muy mala. Lo lamentamos mucho");
+    console.log("Te agradecemos por tu visita");
+    break;
+    case 2: 
+    console.log("Calificaste la pelicula como mala. lo lamentamos mucho");
+    console.log("Te agradecemos por tu visita");
+    break;
+    case 3: 
+    console.log("Calificaste la pelicula como mediocre. lo lamentamos mucho");
+    console.log("Te agradecemos por tu visita");
+    break;
+    case 4: 
+    console.log("Calificaste la pelicula como buena. Genial!!");
+    console.log("Te agradecemos por tu visita");
+    case 5: 
+    console.log("Calificaste la pelicula como muy buena. Espectacular!");
+    console.log("Te agradecemos por tu visita");
+    default:
+        console.log("Ingresaste un valor inválido");
+}
